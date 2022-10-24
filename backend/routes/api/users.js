620b4acf00dba73aa233a529fbf7cdb8802ab768
Isaac <isaacc98@gmail.com>
2022-10-24 16:18:19 -0400

    jwt authentication

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,25 +4,14 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+const { loginUser, restoreUser } = require("../../config/passport");
+const { isProduction } = require("../../config/keys");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.json({
     message: "GET /api/users",
   });
-});
-
-router.post("/login", async (req, res, next) => {
-  passport.authenticate("local", async function (err, user) {
-    if (err) return next(err);
-    if (!user) {
-      const err = new Error("Invalid credentials");
-      err.statusCode = 400;
-      err.errors = { email: "Invalid credentials" };
-      return next(err);
-    }
-    return res.json({ user });
-  })(req, res, next);
 });
 
 router.post("/register", async (req, res, next) => {
@@ -58,11 +47,40 @@ router.post("/register", async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
       } catch (err) {
         next(err);
       }
     });
+  });
+});
+
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("local", async function (err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error("Invalid credentials");
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
+    }
+    return res.json(await loginUser(user));
+  })(req, res, next);
+});
+
+router.get("/current", restoreUser, (req, res) => {
+  if (!isProduction) {
+    // In development, allow React server to gain access to the CSRF token
+    // whenever the current user information is first loaded into the
+    // React application
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email,
   });
 });
 
