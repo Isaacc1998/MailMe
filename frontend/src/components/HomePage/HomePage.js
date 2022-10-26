2549cc8866cf986d2ecd77d5637b519c77bd1e32
Isaac <isaacc98@gmail.com>
2022-10-26 16:31:59 -0400

    fixed modal for create email

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,9 +1,26 @@
 import NavBar from "../NavBar/NavBar";
 import "./HomePage.css";
-import CreateEmailFormModal from "../CreateEmailForm/CreateEmailForm";
-import { useSelector } from "react-redux";
+import CreateEmailFormModal from "../CreateEmailForm/CreateEmailFormModal";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUserMailingLists } from "../../store/mailinglist";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 function HomePage() {
-  // const mailingLists = useSelector(state => state.mailingLists.lists)
+  const dispatch = useDispatch();
+  const mailingLists = useSelector((state) => state.mailingLists.lists);
+  useEffect(() => {
+    dispatch(getUserMailingLists());
+  }, [dispatch]);
   return (
     <>
       <NavBar />
@@ -21,17 +38,65 @@ function HomePage() {
             </div>
           </div>
           <div className="bottom-right-home-container">
-            <div>Bottom Right side container &nbsp;</div>
-            <div>Content of Mailing List</div>
-            {/* {mailingLists.length > 0 &&
-              mailingLists.map((list) => {
-                return (
-                  <>
-                    <div>{list.name}</div>
-                    <div>{JSON.stringify(list.emails)}</div>
-                  </>
-                );
-              })} */}
+            {/* <div>Bottom Right side container &nbsp;</div>
+            <div>Content of Mailing List</div> */}
+            <div>
+              <TableContainer>
+                <Table variant="striped" colorScheme="teal">
+                  <TableCaption>Isaac</TableCaption>
+                  <Thead>
+                    <Tr>
+                      <Th>Mailing List Name</Th>
+                      <Th># of Emails</Th>
+                      {/* <Th isNumeric># of Emails</Th> */}
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {mailingLists.length > 0 &&
+                      mailingLists.map((list) => {
+                        return (
+                          // name of mailing, number of emails contained,
+                          // <div
+                          //   style={{
+                          //     border: "1px solid black",
+                          //     margin: "5px",
+                          //     padding: "5px",
+                          //   }}
+                          // >
+                          <Tr>
+                            <Td>{list.name}</Td>
+
+                            <Td>emails: {list.emails.length}</Td>
+                          </Tr>
+                          // </div>
+                        );
+                      })}
+                    {/* <Tr>
+        <Td>inches</Td>
+        <Td>millimetres (mm)</Td>
+        <Td isNumeric>25.4</Td>
+      </Tr>
+      <Tr>
+        <Td>feet</Td>
+        <Td>centimetres (cm)</Td>
+        <Td isNumeric>30.48</Td>
+      </Tr>
+      <Tr>
+        <Td>yards</Td>
+        <Td>metres (m)</Td>
+        <Td isNumeric>0.91444</Td>
+      </Tr> */}
+                  </Tbody>
+                  {/* <Tfoot>
+      <Tr>
+        <Th>To convert</Th>
+        <Th>into</Th>
+        <Th isNumeric>multiply by</Th>
+      </Tr>
+    </Tfoot> */}
+                </Table>
+              </TableContainer>
+            </div>
           </div>
         </div>
       </div>
