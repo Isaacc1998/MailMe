a5271d255dd19ad151fe18f79081280cbd9d89a2
wc2184 <68214261+wc2184@users.noreply.github.com>
2022-11-01 14:34:08 -0400

    .

--- frontend/src/SubscribeLinkPage/SubscribeLink.js
+++ frontend/src/SubscribeLinkPage/SubscribeLink.js
@@ -1,7 +1,7 @@
 import { Box, Button, Heading, Image, Input, useToast } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getMailingList } from "../store/mailinglist";
+import { getMailingList, updateMailingList } from "../store/mailinglist";
 import "./SubscribeLink.css";
 
 const SubscribeLink = () => {
@@ -12,10 +12,31 @@ const SubscribeLink = () => {
     (state) => state.mailingLists.currentMailingList
   );
   const toast = useToast();
+  const ref = useRef();
 
   useEffect(() => {
     dispatch(getMailingList(mailingListId));
+    ref.current.focus();
   }, []);
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    let obj = {
+      mailinglistId: mailingListId,
+      name: currentMailingList.name,
+      emails: [ref.current.value],
+    };
+    dispatch(updateMailingList(obj)).then(() => {
+      toast({
+        title: "Subscription confirmed.",
+        description: "You have subscribed!.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      ref.current.value = "";
+    });
+  };
   return (
     <Box
       mt="10vh"
@@ -35,22 +56,24 @@ const SubscribeLink = () => {
         Confirm your subscription to{" "}
         {currentMailingList && currentMailingList.name}'s newsletter.
       </Heading>
-      <Input w="700px" placeholder="Enter your email" />
-      <Button
-        colorScheme="green"
-        onClick={() => {
-          toast({
-            title: "Subscription confirmed.",
-            description: "You have subscribed!.",
-            status: "success",
-            duration: 5000,
-            isClosable: true,
-          });
-        }}
-        mt={10}
+
+      <form
+        onSubmit={submitHandler}
+        style={{ display: "flex", flexDirection: "column" }}
       >
-        Confirm Subscription
-      </Button>
+        <Input
+          ref={ref}
+          w="700px"
+          placeholder="Enter your email"
+          required
+          type="email"
+        />
+        <Box textAlign="center">
+          <Button type="submit" colorScheme="green" mt={10}>
+            Confirm Subscription
+          </Button>
+        </Box>
+      </form>
     </Box>
   );
 };
